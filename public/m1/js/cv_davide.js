77a60b2ed35507516cc8aed8238df5ec0ba227b1
Davide <d.depasquale94@gmail.com>
2020-06-18 16:12:00 +0200

    Upload html personal page as exercise

---
+++ public/m1/js/cv_davide.js
@@ -0,0 +1,7 @@
+document.getElementById('backGroundColor').onclick() = () => {
+    document.getElementsByName('body').bgcolor = 'darkgrey';
+}
+
+document.getElementById('changeFont').onclick() = () => { 
+    
+}
